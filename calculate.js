731c38f2f199d43b60b459b400af25bbb709d5a8
tada <rortheford@gmail.com>
2022-12-20 13:08:30 -0800

    Add basic arithemtic operation functions

---
+++ calculate.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1+num2;
+}
+
+function subtract(num1, num2) {
+    return num1-num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function division(num1, num2) {
+    return num1/num2;
+}
+
+function operate(op, num1, num2) {
+    if (op === "+")
+        return add(num1, num2);
+    else if(op === "-")
+        return subtract(num1, num2);
+    else if(op === "*")
+        return multiply(num1, num2);
+    else if (op === "/")
+        return division(num1, num2);
+
+}
