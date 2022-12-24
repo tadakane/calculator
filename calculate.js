86da782c2d6302acf677dff2670cc3879a896f48
tada <rortheford@gmail.com>
2022-12-23 16:03:32 -0800

    Add floating point functionality

--- calculate.js
+++ calculate.js
@@ -38,25 +38,59 @@ function dataInput(input){
     let display = document.querySelector(".display");
     if (op === false) {
         divZero = false;
-        if (data1 == 0 || equals === true) {
-            display.textContent = input;
-            data1 = input;
+        if (data1 === 0 || equals === true) {
+            if (input === -1) {
+                display.textContent = "0."
+                data1 = "0.";
+                dot = true;
+            }
+            else {
+                display.textContent = input;
+                data1 = input;
+            }
             equals = false;
         }
-        else if (String(Math.abs(data1)).length < 10){
-            display.textContent += input;
-            data1 += String(input);
+        else if (String(Math.abs(data1)).replace('.', '').length < 10){
+            if (input === -1 && dot === false) {
+                display.textContent += ".";
+                data1 += ".";
+                dot = true;
+            }
+            else if (input !== -1) {
+                zeroCount++;
+                if (zeroCount < 9 || input !== 0) {
+                    display.textContent += input;
+                    data1 += String(input);
+                }
+            }
         }
     }
     else if (op === true) {
         second = true;
-        if (data2 == 0) {
-            display.textContent = input;
-            data2 = input;
+        if (data2 === 0) {
+            if (input === -1) {
+                display.textContent = "0.";
+                data2 = "0.";
+                dot = true;
+            }
+            else {
+                display.textContent = input;
+                data2 = input;
+            }
         }
-        else if (String(Math.abs(data2)).length < 10){
-            display.textContent += input;
-            data2 += String(input);
+        else if (String(Math.abs(data2)).replace('.', '').length < 10){
+            if (input === -1 && dot === false) {
+                display.textContent += ".";
+                data2 += ".";
+                dot = true;
+            }
+            else if (input !== -1) {
+                zeroCount++
+                if (zeroCount < 9 || input !== 0) {
+                    display.textContent += input;
+                    data2 += String(input);
+                }
+            }
         }
         if (divZero === true) {
             operator = "/";
@@ -70,29 +104,37 @@ function opInput(input) {
     let temp = 0;
     if (input === 10 && divZero === false) {
         if (op === false) {
-            temp = data1 * -1;
-            if (temp < Number.MAX_SAFE_INTEGER && temp > Number.MIN_SAFE_INTEGER)
+            if (parseFloat(data1) < 0)
+                temp = data1.slice(1);
+            else if (parseFloat(data1) > 0)
+                temp = "-" + data1;
+            if (parseFloat(temp) < Number.MAX_SAFE_INTEGER && parseFloat(temp) > Number.MIN_SAFE_INTEGER)
                 display.textContent = data1 = temp;
             checkLength();
         }
         else {
-            temp = data2 * -1;
+            if (parseFloat(data2) < 0)
+                temp = data2.slice(1);
+            else if (parseFloat(data2) > 0)
+                temp = "-" + data2;
             display.textContent = data2 = temp;
         }
     }
     else if (input >= 11 && input <= 14) {
+        dot = false;
+        zeroCount = 0;
         if (op === false) {
             second = false;
             data2 = 0;
         }
         else if (op === true && second === true) {
-            if (operator === "/" && data2 === 0) {
+            if (operator === "/" && data2 == 0) {
                 display.textContent = "No."
                 data1 = 0;
                 divZero = true;
             } 
             else {
-                temp = operate(operator, parseInt(data1), parseInt(data2));
+                temp = operate(operator, parseFloat(data1), parseFloat(data2));
                 if (temp < Number.MAX_SAFE_INTEGER && temp > Number.MIN_SAFE_INTEGER)
                     display.textContent = data1 = temp;
                 checkLength();
@@ -115,13 +157,13 @@ function opInput(input) {
         op = true;
     }
     else if (input === 15 && second === true) {
-        if (operator === "/" && data2 === 0) {
+        if (operator === "/" && data2 == 0) {
             display.textContent = "No."
             data1 = 0;
             divZero = true;
         } 
         else {
-            temp = operate(operator, parseInt(data1), parseInt(data2));
+            temp = operate(operator, parseFloat(data1), parseFloat(data2));
             if (temp < Number.MAX_SAFE_INTEGER && temp > Number.MIN_SAFE_INTEGER)
                 display.textContent = data1 = temp;
             checkLength();
@@ -135,6 +177,8 @@ function opInput(input) {
         second = false;
         operator = "";
         divZero = false;
+        dot = false;
+        zeroCount = 0;
     }
     else if (input === 17) {
         backSpace();
@@ -147,6 +191,8 @@ function backSpace() {
         if (display.textContent.length === 1) {
             data1 = 0;
             display.textContent = 0;
+            dot = false;
+            zeroCount = 0;
         }
         else {
             data1 = String(data1).slice(0, -1);
@@ -157,6 +203,8 @@ function backSpace() {
         if (display.textContent.length === 1) {
             data2 = 0;
             display.textContent = 0;
+            dot = false;
+            zeroCount = 0;
         }
         else {
             data2 = String(data2).slice(0, -1);
@@ -169,18 +217,29 @@ function checkLength() {
     let display = document.querySelector(".display");
     let maxLength;
     let sciNotat = {};
-    if (parseInt(display.textContent) < 0)
+    if (parseFloat(display.textContent) < 0)
         maxLength = 11;
     else 
         maxLength = 10;
     
-    if (display.textContent.length > maxLength) {
-        [sciNotat.coefficient, sciNotat.exponent] = data1.toExponential().split('e+');
-        display.textContent =
-            parseFloat(sciNotat.coefficient).toFixed(10 - 
-                    (sciNotat.exponent.toString().length)-2) +
-                    "e" + sciNotat.exponent;
-        data1 = parseFloat(sciNotat.coefficient) * (10 ** parseInt(sciNotat.exponent));    
+    if (display.textContent.replace('.', '').length > maxLength) {
+        if (parseFloat(data1) < 1 && parseFloat(data1) > 0 ||
+            parseFloat(data1) < 0 && parseFloat(data1) > -1) {
+            [sciNotat.coefficient, sciNotat.exponent] = parseFloat(data1).toExponential().split('e-');
+            display.textContent =
+                parseFloat(sciNotat.coefficient).toFixed(10 - 
+                        (sciNotat.exponent.toString().length)-3) +
+                        "e-" + sciNotat.exponent;
+            data1 = parseFloat(sciNotat.coefficient) / (10 ** parseInt(sciNotat.exponent));   
+        }
+        else {
+            [sciNotat.coefficient, sciNotat.exponent] = parseFloat(data1).toExponential().split('e+');
+            display.textContent =
+                parseFloat(sciNotat.coefficient).toFixed(10 - 
+                        (sciNotat.exponent.toString().length)-3) +
+                        "e+" + sciNotat.exponent;
+            data1 = parseFloat(sciNotat.coefficient) * (10 ** parseInt(sciNotat.exponent));   
+        }
     }
 }
 
@@ -191,6 +250,8 @@ let second = false;
 let operator = "";
 let divZero = false;
 let equals = false;
+let dot = false;
+let zeroCount = 0;
 
 const nine = document.querySelector("#nine");
 nine.addEventListener('click', () => changeDisplay(9));
@@ -228,4 +289,5 @@ const clear = document.querySelector("#clear");
 clear.addEventListener('click', () => changeDisplay(16));
 const back = document.querySelector("#back");
 back.addEventListener('click', () => changeDisplay(17));
-
+const decimal = document.querySelector("#decimal");
+decimal.addEventListener('click', () => changeDisplay(-1));
